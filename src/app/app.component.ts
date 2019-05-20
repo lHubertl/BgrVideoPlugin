515dab98fcde780be3c4ee99cff3aa7dba831f22
Sovytskyi Valerii <valerii.sovytskyi@jetsoftpro.com>
2019-05-20 13:41:18 +0300

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+declare var cordova: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -18,6 +20,15 @@ export class AppComponent {
   }
 
   initializeApp() {
+
+    var success = function(result) {
+      alert(JSON.stringify(result, undefined, 2));
+    }
+    var failure = function(result) {
+      alert(JSON.stringify(result, undefined, 2));
+    }
+    cordova.plugins.BgrVideoPlugin.Start(null, null, null, success, failure);
+
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
